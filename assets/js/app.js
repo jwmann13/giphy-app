42d65c536a6bd769c7da2ec106558157e4492250
Jeffrey Mann <jwmann22@gmail.com>
2019-09-25 14:23:29 -0400

    gif click animation event working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
     let topics = ["penguins", "cats", "owls", "hamsters", "dogs", "ducks"];
-    let apiKey = "QOFN0QbeSz8WpxqX33mOir8ublOe8djd";
 
     for (let i = 0; i < topics.length; i++) {
         const element = topics[i];
@@ -8,20 +7,40 @@ $(document).ready(() => {
         button.text(element);
         $('#buttons').append(button);
     }
+
+    $('.container').on('click', '.gif', function (event) {
+        // console.log(event);
+        // console.log('clicked', $(this));
+        let state = $(this).data('state');
+        if (state === "still") {
+            $(this).attr('src', $(this).data('animate')).data('state', 'animate');
+        } else if (state === 'animate') {
+            $(this).attr('src', $(this).data('still')).data('state', 'still');
+        }
+    });
+
     $('button').on('click', function (event) {
+        let apiKey = "QOFN0QbeSz8WpxqX33mOir8ublOe8djd";
         let queryURL = `https://api.giphy.com/v1/gifs/search?limit=10&api_key=${apiKey}&q=${$(this).data('topic')}`;
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+            $('#gifs').empty();
             for (let i = 0; i < response.data.length; i++) {
                 const element = response.data[i];
-                let imgURL = element.images.fixed_height.url;
-                console.log(imgURL)
-                let image = $('<img>').attr('src', imgURL);
+                let imgURLStill = element.images.fixed_height_still.url;
+                let imgURLAnimate = element.images.fixed_height.url;
+                // console.log(imgURLStill)
+                let image = $('<img>').addClass('gif').attr({
+                    'src': imgURLStill,
+                    'data-still': imgURLStill,
+                    'data-animate': imgURLAnimate,
+                    'data-state': 'still'
+                });
                 $('#gifs').append(image);
-                console.log(response);
+                // console.log(response);
             }
-        })
-    })
-})
+        });
+    });
+});
