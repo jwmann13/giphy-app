71845d9fc1e752394a953055e62f3da911a5143a
Jeffrey Mann <jwmann22@gmail.com>
2019-10-01 19:29:18 -0400

    still working on scroller

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,38 @@
 $(document).ready(() => {
     let topics = ["penguins", "cats", "owls", "hamsters", "dogs", "ducks"];
     renderButtons(topics);
-    // initIntersectionObserver();
 
     // click listener to pause/play gifs
     $('#gifs').on('click', '.gifcontainer,.gif,.text', function (event) {
-        let clicked = $(this);
-        let state = clicked.data('state');
+        let clickedItem = $(this);
+        let state = clickedItem.data('state');
+
         // handles if image is clicked directly
-        if (clicked[0].nodeName == 'IMG') {
-            console.log('hello');
+        if (clickedItem[0].nodeName == 'IMG') {
+
             if (state === "still") {
-                clicked.attr('src', clicked.data('animate')).data('state', 'animate');
+                clickedItem.attr('src', clickedItem.data('animate')).data('state', 'animate');
             } else if (state === 'animate') {
-                clicked.attr('src', clicked.data('still')).data('state', 'still');
+                clickedItem.attr('src', clickedItem.data('still')).data('state', 'still');
             }
-        // handles if description div is clicked
-        } else if (clicked[0].nodeName == 'DIV') {
-            let image = $(clicked[0].parentElement.parentElement.firstChild);
-            state = image.data('state')
-            console.log('goodbye', image);
+
+            // handles if description div is clicked
+        } else if (clickedItem[0].nodeName == 'DIV') {
+
+            let image = $(clickedItem[0].parentElement.parentElement.firstChild);
+            state = image.data('state');
             if (state === "still") {
                 image.attr('src', image.data('animate')).data('state', 'animate');
             } else if (state === 'animate') {
                 image.attr('src', image.data('still')).data('state', 'still');
             }
+
         }
     });
 
     // click listener to load gifs
     $('#buttons').on('click', '.gifBtn', function (event) {
-        let apiKey = "QOFN0QbeSz8WpxqX33mOir8ublOe8djd";
+        const apiKey = "QOFN0QbeSz8WpxqX33mOir8ublOe8djd";
         let queryURL = `https://api.giphy.com/v1/gifs/search?limit=30&api_key=${apiKey}&q=${$(this).data('topic')}`;
         $.ajax({
             url: queryURL,
@@ -38,6 +40,7 @@ $(document).ready(() => {
         }).then(function (response) {
             renderGifs(response)
         });
+        console.log(section)
     });
 
     // click listener to create new submission buttons
@@ -51,11 +54,12 @@ $(document).ready(() => {
         $('#newButton').val('');
     });
 
-    $('#gifs').on('scroll', function (event) {
-        console.log(event.target)
-    })
+    // $('#gifs').on('scroll', function (event) {
+    //     console.log(event.target)
+    // })
 });
 
+// createButton() adds button to #buttons container; renderButtons() displays #buttons
 function renderButtons(entries) {
     $('#buttons').empty();
     for (let i = 0; i < entries.length; i++) {
@@ -72,22 +76,22 @@ function createButton(input, index) {
     $('#buttons').append(button);
 }
 
+// createGifDiv() creates containers for gif img and description text then adds them to #gifs; renderGifs() displays #gifs
 function renderGifs(input) {
     $('#gifs').empty();
     for (let i = 0; i < input.data.length; i++) {
         const element = input.data[i];
         createGifDiv(element, i);
-        // console.log(input);
     }
 }
 
 function createGifDiv(element, index) {
-    let imgURLStill = element.images.fixed_width_still.url;
-    let imgURLAnimate = element.images.fixed_width.url;
-    let rating = element.rating;
-    let source = element.source;
-    let title = parseGifTitle(element.title);
-    // console.log(element)
+    const imgURLStill = element.images.fixed_width_still.url;
+    const imgURLAnimate = element.images.fixed_width.url;
+    const rating = element.rating;
+    const source = element.source;
+    const title = parseGifTitle(element.title);
+
     let gifContainer = $('<div>').addClass('gifContainer').attr('id', 'gif-container-' + index);
     let image = $('<img>').addClass('gif').attr({
         'src': imgURLStill,
@@ -95,7 +99,8 @@ function createGifDiv(element, index) {
         'data-animate': imgURLAnimate,
         'data-state': 'still'
     });
-    let overlay = $('<div>').addClass('overlay').append($('<div>').addClass('text').html(title + 'rated: ' + rating + '</br><a href="' + source + '">Source</a>'));
+    let overlay = $('<div>').addClass('overlay').append($('<div>').addClass('text').html(title + 'rated: ' + rating + '</br><a href="' + source + '" target="_blank" rel="noopener noreferrer">Source</a>'));
+
     gifContainer.append(image, overlay).appendTo('#gifs');
 }
 
@@ -111,4 +116,8 @@ function parseGifTitle(title) {
         attribution = '"' + attribution + '"</br>';
     }
     return attribution;
+}
+
+function stopAllGifs() {
+
 }
--- assets/js/scroller.js
+++ assets/js/scroller.js
@@ -1,44 +1,18 @@
-const initIntersectionObserver = () => {
-  const options = {
-    root: null
-    // root: document.querySelector('#gifs')
-    /* root: document.querySelector(".cat-list") */
-  }
+const header = document.querySelector('.input')
+const section = $('#gifs')[0].children;
+const topSentinel = section[0];
+const bottomSentinel = section[section.length-1];
 
-  const callback = entries => {
-    entries.forEach(entry => {
-      console.log(entry.target)
-      if (entry.target.id === 'gif-container-0') {
-        topSentCallback(entry);
-      } else if (entry.target.id === `gif-container-${listSize - 1}`) {
-        botSentCallback(entry);
-      }
-    });
-  }
+const sectionObserver = new IntersectionObserver(function(entries, sectionObserver){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting){
+      console.log('fart');
+    } else {
+      console.log('poot');
+    }
+  })
+});
 
-  var observer = new IntersectionObserver(callback, options);
-  observer.observe(document.querySelector("#gif-container-0"));
-  // observer.observe($(`#gif-container-${listSize - 1}`));
-}
+// sectionObserver.observe(topSentinel);
 
-const recycleDOM = firstIndex => {
-  for (let i = 0; i < listSize; i++) {
-    const tile = document.querySelector("#gif-container-" + i);
-    tile.firstElementChild.innerText = DB[i + firstIndex].title;
-    tile.lastChild.setAttribute("src", DB[i + firstIndex].imgSrc);
-  }
-}
-
-const adjustPaddings = isScrollDown => {
-  const container = document.querySelector("#gifs");
-  const currentPaddingTop = getNumFromStyle(container.style.paddingTop);
-  const currentPaddingBottom = getNumFromStyle(container.style.paddingBottom);
-  const remPaddingsVal = 170 * (listSize / 2);
-  if (isScrollDown) {
-    container.style.paddingTop = currentPaddingTop + remPaddingsVal + "px";
-    container.style.paddingBottom = currentPaddingBottom === 0 ? "0px" : currentPaddingBottom - remPaddingsVal + "px";
-  } else {
-    container.style.paddingBottom = currentPaddingBottom + remPaddingsVal + "px";
-    container.style.paddingTop = currentPaddingTop === 0 ? "0px" : currentPaddingTop - remPaddingsVal + "px";
-  }
-}
+console.log(topSentinel);
